6e10f03a8308ea2b103daf568a9b1e020c37e2d5
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2023-10-20 16:38:00 +0200

    cache skeleton

--- RizSoft.SiteThatRedirects/MyRedirectRule.cs
+++ RizSoft.SiteThatRedirects/MyRedirectRule.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Net.Http.Headers;
 using System.Net;
 using System.Text.Json;
@@ -15,21 +16,30 @@ namespace RizSoft.SiteThatRedirects
             var request = context.HttpContext.Request;
             var host = request.Host;
 
-            string fileName = "UrlList.json";
-            using FileStream openStream = File.OpenRead(fileName);
-            var urls = JsonSerializer.Deserialize<List<UrlRedir>>(openStream);
+            string filename = "UrlList.json";
+            if (File.Exists(filename))
+            {       
+                using FileStream openStream = File.OpenRead(filename);
+                var urls = JsonSerializer.Deserialize<List<UrlRedir>>(openStream);  
 
-            //TODO cache after 1st load
+                //TODO cache after 1st load
+                // how to inject Imemorycache??
+                //if (!_memoryCache.TryGetValue("myKey", out List<UrlRedir> urls))
+                //{
+                //    urls = JsonSerializer.Deserialize<List<UrlRedir>>(openStream);
+                //    _memoryCache.Set("myKey", urls, TimeSpan.FromMinutes(5)); // Cache for 5 minutes
+                //}
 
-            if (urls != null)
-            {
-                var url = urls.Find(x => x.Path == request.Path.ToString().ToLower());
-                if (url != null)
+                if (urls != null)
                 {
-                    var response = context.HttpContext.Response;
-                    response.StatusCode = StatusCode;
-                    response.Headers[HeaderNames.Location] = url.NewUrl;
-                    context.Result = RuleResult.EndResponse; // Do not continue processing the request
+                    var url = urls.Find(x => x.Path == request.Path.ToString().ToLower());
+                    if (url != null)
+                    {
+                        var response = context.HttpContext.Response;
+                        response.StatusCode = StatusCode;
+                        response.Headers[HeaderNames.Location] = url.NewUrl;
+                        context.Result = RuleResult.EndResponse; // Do not continue processing the request
+                    }
                 }
             }
         }
--- RizSoft.SiteThatRedirects/Program.cs
+++ RizSoft.SiteThatRedirects/Program.cs
@@ -4,13 +4,15 @@ using RizSoft.SiteThatRedirects;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//builder.Services.AddMemoryCache();
+
 app.UseRewriter(new RewriteOptions().Add(new MyRedirectRule()));
 
 app.UseStaticFiles();
 
 app.MapGet("/", () => "Home of Redirect Site"); //this works ony if you don't have a record with path = "/"
 
-//only for debug
+//only for debug purpose
 app.MapGet("/give-me-request", (HttpRequest request) =>
 {
     return $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
